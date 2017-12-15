02e875cdda3c9579dfc8a76bad21f1af45d1d63a
Maxx84 <massimo.vespignani@gmail.com>
2017-12-15 10:50:15 -0800

    SineWave controller modified to run battery voltage experiments. The controller will run a simple sine wave on each Hebi X8-3 on the network. Voltage level is plotted and printed every 5 seconds, and logged at 100Hz throughout the whole experiment. The user should press 'q' while focused on the plot to properly quit the program.

--- SineWaveController/SineWaveController.m
+++ SineWaveController/SineWaveController.m
@@ -1,8 +1,11 @@
 %% Simple Hebi Sine Wave Controller
 % Version 0.1 - 12/13/2017
 
-% The program commands a simple sine wave to the Hebi X8-3 motors.
-% Amplitude, Frequency, and Phase can be changed.
+% The program commands a simple sine wave to the Hebi X8-3 motors and keeps
+% a log of the data. Amplitude, Frequency, and Phase can be changed. 
+
+% The program can be started by inputting 's' in the console, and stopped
+% by pressing 'q' while the focus is on the plot window.
 
 % In order to run, add to path the latest Hebi Matlab API, available at
 % http://docs.hebi.us/
@@ -21,7 +24,10 @@ nbMotors = Group.getNumModules;
 
 amplitude = 5.0; % [rad]
 frequency = 0.3; % [Hz]
-phase = 0.0; % []
+
+for jj = 1:nbMotors
+    phase(jj) = pi * (jj-1); % [rad]
+end
 
 %% Go to initial position
 
@@ -50,16 +56,29 @@ disp('Logging Initiated.');
 notQuit = 1;
 set(gcf,'CurrentCharacter','@'); % set to a dummy character
 
-globalTime = tic;
-displayTime = tic;
+globalTime = tic; % Used for the sinusoidal motion
+displayTime = tic; % Used to display data at regular intervals
 
 h = figure();
+
+currFbk = Group.getNextFeedback;
+disp(['Battery voltage: ' num2str(currFbk.voltage) 'V. ' 'Elapsed Time: ' num2str(toc(globalTime)) 's.']);
+
+% Plot the voltage (from the first motor)
+scatter(toc(globalTime), currFbk.voltage(1), 'filled', 'k')
+hold on
+
+ylim([0 30]);
+xlim([0 toc(globalTime)+ 5]);
+
+xlabel('Time [s]');
+ylabel('Voltage [V]');
 
 while notQuit
     
     % Sine Wave
     for i=1:nbMotors
-        cmdMotorPositions(i) = amplitude * sin(2*pi*frequency*toc(globalTime) + phase);
+        cmdMotorPositions(i) = amplitude * sin(2*pi*frequency*toc(globalTime) + phase(i));
     end
     
     % Send new positions to motors
@@ -74,7 +93,18 @@ while notQuit
         currFbk = Group.getNextFeedback();
         disp(['Battery voltage: ' num2str(currFbk.voltage) 'V. ' 'Elapsed Time: ' num2str(toc(globalTime)) 's.']);
         displayTime = tic;
+        
+        % Live plot data - voltage
+        scatter(toc(globalTime), currFbk.voltage(1), 'filled', 'k')
+
+        xlim([0 toc(globalTime)+ 5]);
+
+        %drawnow;
     end
+    
+    
+
+    pause(0.005);
     
 
     % check for quit key
